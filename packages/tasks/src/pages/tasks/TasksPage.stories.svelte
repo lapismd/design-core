2c2422e4f5a6457bae325a3a1a0deff02b9d14e3
Steve Juma <dev@ju.ma>
2026-07-20 23:11:02 +0100

    Add Tasks overview page composition

---
+++ packages/tasks/src/pages/tasks/TasksPage.stories.svelte
@@ -0,0 +1,36 @@
+<script module lang="ts">
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import { expect, userEvent } from "storybook/test";
+  import Page from "./TasksPage.svelte";
+
+  const { Story } = defineMeta({
+    title: "Tasks/Pages/Tasks",
+    component: Page,
+    tags: ["skip-visual"],
+    parameters: {
+      layout: "fullscreen",
+      docs: {
+        description: {
+          component: "Fixture-driven Tasks page composition.",
+        },
+      },
+    },
+  });
+</script>
+
+<script lang="ts">
+  import { referenceVisualDelta } from "../../lib/story-data.js";
+</script>
+
+<Story
+  name="Default"
+  exportName="Default"
+  parameters={{ visualDelta: referenceVisualDelta("desktop-tasks") }}
+  play={async ({ canvas }) => {
+    await expect(canvas.getByText("Tasks")).toBeVisible();
+  }}
+>
+  {#snippet template()}
+    <Page />
+  {/snippet}
+</Story>
