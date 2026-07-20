4883aa4d857b6d28539ae93b5d07c845f815d41b
Steve Juma <dev@ju.ma>
2026-07-20 23:11:03 +0100

    Add List detail page composition

---
+++ packages/tasks/src/pages/list-detail/ListDetailPage.stories.svelte
@@ -0,0 +1,53 @@
+<script module lang="ts">
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import { expect, userEvent } from "storybook/test";
+  import Page from "./ListDetailPage.svelte";
+
+  const { Story } = defineMeta({
+    title: "Tasks/Pages/List detail",
+    component: Page,
+    tags: ["skip-visual"],
+    parameters: {
+      layout: "fullscreen",
+      docs: {
+        description: {
+          component: "Fixture-driven List detail page composition.",
+        },
+      },
+    },
+  });
+</script>
+
+<script lang="ts">
+  import { referenceVisualDelta } from "../../lib/story-data.js";
+</script>
+
+<Story
+  name="Default"
+  exportName="Default"
+  parameters={{ visualDelta: referenceVisualDelta("desktop-list-detail") }}
+  play={async ({ canvas }) => {
+    await expect(canvas.getByText("List detail")).toBeVisible();
+  }}
+>
+  {#snippet template()}
+    <Page />
+  {/snippet}
+</Story>
+
+<Story
+  name="Open detail"
+  exportName="OpenDetail"
+  parameters={{ visualDelta: referenceVisualDelta("desktop-task-detail") }}
+  play={async ({ canvas }) => {
+    await userEvent.click(
+      canvas.getByRole("button", { name: "Review the launch brief" }),
+    );
+    await userEvent.click(canvas.getByRole("button", { name: "Details" }));
+    await expect(canvas.getByText("Detail open task-brief")).toBeVisible();
+  }}
+>
+  {#snippet template()}
+    <Page />
+  {/snippet}
+</Story>
