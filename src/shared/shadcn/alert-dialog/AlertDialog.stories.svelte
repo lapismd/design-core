c07860a6bb03e402cb3a9179ef302dfde0acada9
Steve Juma <dev@ju.ma>
2026-07-30 23:25:02 +0100

    Visual Delta: auto accept shadcn-overlays-alert-dialog--open-alert-dialog

--- src/shared/shadcn/alert-dialog/AlertDialog.stories.svelte
+++ src/shared/shadcn/alert-dialog/AlertDialog.stories.svelte
@@ -95,7 +95,7 @@
 
 <Story
   name="Open alert dialog"
-  tags={["visual-state", "visual-pending"]}
+  tags={["visual-state", "visual-approved"]}
   play={async ({ canvas }) => {
     await dismissOverlays();
     await userEvent.click(canvas.getByRole("button", { name: "Show Dialog" }));
