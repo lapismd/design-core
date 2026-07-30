b2a79b0b7fe449734b9c9abaf8fed11aeb9992e1
Steve Juma <dev@ju.ma>
2026-07-30 23:26:26 +0100

    Visual Delta: auto accept shadcn-overlays-dialog--open-custom-close-dialog

--- src/shared/shadcn/dialog/Dialog.stories.svelte
+++ src/shared/shadcn/dialog/Dialog.stories.svelte
@@ -109,7 +109,7 @@
 
 <Story
   name="Open custom close dialog"
-  tags={["visual-state", "visual-pending"]}
+  tags={["visual-state", "visual-approved"]}
   play={async ({ canvas }) => {
     await dismissOverlays();
     await userEvent.click(canvas.getByRole("button", { name: "Share" }));
