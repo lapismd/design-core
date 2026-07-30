e7a3ccbf73a3a15f0cabb6328c8c1a29ca5ddffe
Steve Juma <dev@ju.ma>
2026-07-30 22:20:01 +0100

    Visual Delta: auto accept filter-search-filter-bar--filter-query-autocomplete

--- src/shared/filter/search-filter-bar/SearchFilterBar.stories.svelte
+++ src/shared/filter/search-filter-bar/SearchFilterBar.stories.svelte
@@ -125,7 +125,7 @@
     visualDelta: {"images":["/visual-baselines/filter/search-filter-bar/filter-query-autocomplete-chromium-darwin.png"],"opacity":0.5,"colorInversion":false,"align":"canvas","placement":"right","passThresholdPercent":0.1},
   }}
 
-  tags={["visual-pending"]}
+  tags={["visual-approved"]}
 >
   {#snippet template()}
     <div class="max-w-2xl">
