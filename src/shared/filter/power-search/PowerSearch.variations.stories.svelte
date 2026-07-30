bc62c5a342721845f165f646dbe22ffbc3e989b7
Steve Juma <dev@ju.ma>
2026-07-30 19:44:03 +0100

    Visual Delta: baseline create filter-power-search--content-search-enter

--- src/shared/filter/power-search/PowerSearch.variations.stories.svelte
+++ src/shared/filter/power-search/PowerSearch.variations.stories.svelte
@@ -67,7 +67,11 @@
     });
     expect(canvas.getByText("groceries")).toBeInTheDocument();
   }}
-  tags={["visual-failed"]}
+  tags={["visual-pending"]}
+
+  parameters={{
+    visualDelta: {"images":["/visual-baselines/filter/power-search/content-search-enter-chromium-darwin.png"],"opacity":0.5,"colorInversion":false,"align":"canvas","placement":"right","passThresholdPercent":0.1},
+  }}
 >
   {#snippet template()}
     <div class="max-w-2xl">
