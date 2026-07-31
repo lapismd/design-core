2fbc57ed34d9c69f97756b2afe5ac7dd283e22f6
Steve Juma <dev@ju.ma>
2026-07-31 08:55:39 +0100

    Visual Delta: auto accept shadcn-navigation-pagination--navigates-pages

--- src/shared/shadcn/pagination/Pagination.stories.svelte
+++ src/shared/shadcn/pagination/Pagination.stories.svelte
@@ -29,7 +29,7 @@
     );
     await expect(canvas.getByText("Page 3 of 10")).toBeVisible();
   }}
-  tags={["visual-pending"]}
+  tags={["visual-approved"]}
   parameters={{
     visualDelta: {
       images: [
